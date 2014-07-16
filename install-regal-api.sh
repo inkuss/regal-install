e9b2a1d9a34c0d98f86a0ea33ae0714b92d90cc1
Jan Schnasse <schnasse@gmx.de>
2014-07-16 11:34:53 +0200

    clone from jschnasse repo for testing

--- install-regal-api.sh
+++ install-regal-api.sh
@@ -2,13 +2,10 @@
 
 source variables.conf
 
-git clone https://github.com/edoweb/regal-api.git $ARCHIVE_HOME/regal-api
-git clone https://github.com/edoweb/regal-import.git $ARCHIVE_HOME/regal-import
+git clone https://github.com/jschnasse/regal-api.git $ARCHIVE_HOME/regal-api
+git clone https://github.com/jschnasse/regal-import.git $ARCHIVE_HOME/regal-import
 
 cd $ARCHIVE_HOME/regal-api
 mvn clean install -DskipTests >> $ARCHIVE_HOME/logs/regal-build.log
-cd -
-
 cd $ARCHIVE_HOME/regal-import
 mvn clean install -DskipTests >> $ARCHIVE_HOME/logs/regal-build.log
-cd -
