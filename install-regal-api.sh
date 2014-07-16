78d7a3777860c712d493712d458af7d83575a420
Jan Schnasse <schnasse@gmx.de>
2014-07-16 13:35:20 +0200

    checkout 0.5.0 branch

--- install-regal-api.sh
+++ install-regal-api.sh
@@ -6,8 +6,10 @@ git clone https://github.com/jschnasse/regal-api.git $ARCHIVE_HOME/regal-api
 git clone https://github.com/jschnasse/regal-import.git $ARCHIVE_HOME/regal-import
 
 cd $ARCHIVE_HOME/regal-api
+git checkout 0.5.0
 mvn clean install -DskipTests >> $ARCHIVE_HOME/logs/regal-build.log
 cd -
+git checkout 0.5.0
 cd $ARCHIVE_HOME/regal-import
 mvn clean install -DskipTests >> $ARCHIVE_HOME/logs/regal-build.log
 cd -
