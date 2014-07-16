1c54ba009ae7a6fe67ff8b5dcbdd8cda3c7fb8c8
Jan Schnasse <schnasse@gmx.de>
2014-07-16 13:59:48 +0200

    set PLAY_HOME env doesn't work

--- install-play.sh
+++ install-play.sh
@@ -17,4 +17,3 @@ else
 	unzip play-2.2.3.zip -d $ARCHIVE_HOME
 fi
 
-export PLAY2_HOME=$ARCHIVE_HOME/play-2.2.3
--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -3,5 +3,5 @@
 source variables.conf
 
 cd $ARCHIVE_HOME/regal-api
-$PLAY_HOME/play test
+/home/travis/opt/regal/play-2.2.3/play test
 cd -
