43c540e79fb796709774b1a2c657600c21c04c55
Jan Schnasse <schnasse@gmx.de>
2014-07-22 07:53:09 +0200

    the api is now installed in regal-server

--- copy-config.sh
+++ copy-config.sh
@@ -7,8 +7,8 @@ function copyConfig()
 cp $ARCHIVE_HOME/conf/tomcat-users.xml $TOMCAT_CONF
 cp $ARCHIVE_HOME/conf/fedora-users.xml $ARCHIVE_HOME/fedora/server/config/
 cp $ARCHIVE_HOME/conf/setenv.sh $TOMCAT_HOME/bin
-cp $ARCHIVE_HOME/conf/application.conf $ARCHIVE_HOME/regal-api/conf
-echo "Please execute the following commands manualy"
+cp $ARCHIVE_HOME/conf/application.conf $ARCHIVE_HOME/regal-server/conf
+echo "Please execute the following commands manually"
 echo "sudo cp $ARCHIVE_HOME/conf/elasticsearch.yml $ELASTICSEARCH_CONF"
 }
 
