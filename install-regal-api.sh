92ec88284a1ba7dc0abee17bcd0b712248e62459
Jan Schnasse <schnasse@gmx.de>
2014-09-17 12:15:12 +0200

    change branch to 0.6.0

--- install-regal-api.sh
+++ install-regal-api.sh
@@ -12,7 +12,7 @@ git clone https://github.com/jschnasse/regal-import.git $ARCHIVE_HOME/regal-impo
 fi
 
 cd $ARCHIVE_HOME/regal-api
-git checkout 0.6.0
+git checkout $VERSION
 mvn clean install -DskipTests >> $ARCHIVE_HOME/logs/regal-build.log
 $ARCHIVE_HOME/play-2.2.3/play dist
 cd -
