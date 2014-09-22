37f6dc91a304882d5a685bbcd9619bfe6c6dda7b
Jan Schnasse <schnasse@gmx.de>
2014-09-22 09:59:12 +0200

    copy application.conf to regal-server too

--- copy-config.sh
+++ copy-config.sh
@@ -8,7 +8,7 @@ cp $ARCHIVE_HOME/conf/tomcat-users.xml $TOMCAT_CONF
 cp $ARCHIVE_HOME/conf/fedora-users.xml $ARCHIVE_HOME/fedora/server/config/
 cp $ARCHIVE_HOME/conf/fedora.fcfg $ARCHIVE_HOME/fedora/server/config/
 cp $ARCHIVE_HOME/conf/setenv.sh $TOMCAT_HOME/bin
-#cp $ARCHIVE_HOME/conf/application.conf $ARCHIVE_HOME/regal-server/conf
+cp $ARCHIVE_HOME/conf/application.conf $ARCHIVE_HOME/regal-server/conf
 cp $ARCHIVE_HOME/conf/application.conf $ARCHIVE_HOME/regal-api/conf
 
 echo "Please execute the following commands manually"
