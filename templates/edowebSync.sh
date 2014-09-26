87597fe9d313ba3ba044c8517be78c826332c963
Jan Schnasse <schnasse@gmx.de>
2014-09-26 16:32:24 +0200

    write to stdout

--- templates/edowebSync.sh
+++ templates/edowebSync.sh
@@ -7,6 +7,6 @@ cd $ARCHIVE_HOME/sync
 
 cp .oaitimestamp$NAMESPACE oaitimestamp${NAMESPACE}`date +"%Y%m%d"`
 
-java -jar -Xms512m -Xmx512m ${MODULE}sync.jar --mode PIDL -list $ARCHIVE_HOME/sync/pidlist.txt --user $USER --password $PASSWORD --dtl $DOWNLOAD --cache $ARCHIVE_HOME/${NAMESPACE}base --oai  $OAI --set $SET --timestamp .oaitimestamp$NAMESPACE --fedoraBase http://$SERVER:$TOMCAT_PORT/fedora --host http://$BACKEND --namespace $NAMESPACE >> edoweblog`date +"%Y%m%d"`.txt 2>&1
+java -jar -Xms512m -Xmx512m ${MODULE}sync.jar --mode PIDL -list $ARCHIVE_HOME/sync/pidlist.txt --user $USER --password $PASSWORD --dtl $DOWNLOAD --cache $ARCHIVE_HOME/${NAMESPACE}base --oai  $OAI --set $SET --timestamp .oaitimestamp$NAMESPACE --fedoraBase http://$SERVER:$TOMCAT_PORT/fedora --host http://$BACKEND --namespace $NAMESPACE 
 
 cd -
