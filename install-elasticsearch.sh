665e92c960ed03caae507d16cca719b6c5924b2f
Jan Schnasse <schnasse@gmx.de>
2014-10-31 13:11:51 +0100

    introduc --force-confnew to elasticsearch install - add WARNING NOTE

--- install-elasticsearch.sh
+++ install-elasticsearch.sh
@@ -7,6 +7,10 @@ else
     wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.0.deb
 fi
 echo "Install Elasticsearch"
-sudo dpkg -i elasticsearch-1.1.0.deb
+echo "WARNING: if Elasticsearch is already installed the next command will"
+echo "force to install 1.1.0"
+echo "using sudo dpkg -i --force-confnew elasticsearch-1.1.0.deb"
+echo "Please press cntrl+C to abort"
+sudo dpkg -i --force-confnew elasticsearch-1.1.0.deb
 echo "Start Elasticsearch"
 sudo service elasticsearch start
