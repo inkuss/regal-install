53791f8aca6b84e7d665cac4c94ca93b5f8f996c
Jan Schnasse <jan@schnasse.eu>
2015-06-29 17:27:46 +0200

    add jobdirectory to heritrix start params

--- templates/heritrix-start.sh
+++ templates/heritrix-start.sh
@@ -2,4 +2,4 @@
 
 export HERITRIX_HOME=$ARCHIVE_HOME/heritrix
 
-./bin/heritrix -aadmin:$PASSWORD -p 8443 -b / --ssl-params $ARCHIVE_HOME/conf/regal_keystore,'$PASSWORD'123,'$PASSWORD'123
+./bin/heritrix -aadmin:$PASSWORD -p 8443 -b / --ssl-params $ARCHIVE_HOME/conf/regal_keystore,'$PASSWORD'123,'$PASSWORD'123 --jobs-dir $ARCHIVE_HOME/heritrix-data/
