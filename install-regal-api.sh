4b3bbbb1b0ceaf3cab8ee72e9f59cedb9d79cd97
Jan on Test <schnasse@gmx.de>
2015-09-03 14:25:12 +0200

    just create new app

--- install-regal-api.sh
+++ install-regal-api.sh
@@ -2,6 +2,8 @@
 
 source variables.conf
 
+PORT=$1
+
 if [ ! -d $ARCHIVE_HOME/regal-api ]
 then
 git clone https://github.com/edoweb/regal-api.git $ARCHIVE_HOME/regal-api 
@@ -17,9 +19,12 @@ $ARCHIVE_HOME/activator-dist-1.3.5/activator dist
 cd -
 
 cd $ARCHIVE_HOME
-kill `cat $ARCHIVE_HOME/regal-server/RUNNING_PID`
 unzip regal-api/target/universal/regal-api-*zip -d tmp
-mv regal-server $ARCHIVE_HOME/regal-server.`date  +"%Y%m%d%H%M"`
-mv tmp/regal-api* regal-server
+regalServerDir=regal-server.`date  +"%Y%m%d%H%M"`
+mv tmp/regal-api* $regalServerDir
 rm -rf tmp
+sed s/"^http\.port=.*$"/"http\.port=\$PORT"/ regal-server/conf/application.conf > $regalServerDir/conf/application.conf
+echo "New executable available under $regalServerDir. Provide port info at $regalServerDir/conf/application.conf."
 cd -
+
+
