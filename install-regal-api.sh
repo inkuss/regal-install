2b307b9429762aae0e6ece576ee8c384231ea1e0
Jan on Test <schnasse@gmx.de>
2015-09-08 12:04:04 +0200

    change install routine in install-regal-api.sh - the script installs current snapshot in a newly created directory and creates as softling called regal-server. The application is configured to run on port 9000 or on port 9100. The port is configured with regards to what is currently used. - after installation the new build can be started using the system startscript which should point to the regal-server softlink - with loadCache.sh a prototypical script for loading the applications cache has been added. - if the application is up and running it is only a matter of reloading your webserver to switch between old and new deployment - eventually update your webserver to point your proxy to the new port - last but not least kill the old instance - the script assumes regal-server to be a softlink. It also assumes that the variable http.port exists in regal-server/conf/application.conf

--- install-regal-api.sh
+++ install-regal-api.sh
@@ -2,7 +2,6 @@
 
 source variables.conf
 
-PORT=$1
 
 if [ ! -d $ARCHIVE_HOME/regal-api ]
 then
@@ -19,12 +18,38 @@ $ARCHIVE_HOME/activator-dist-1.3.5/activator dist
 cd -
 
 cd $ARCHIVE_HOME
-unzip regal-api/target/universal/regal-api-*zip -d tmp
+
+OLDDIR=`readlink regal-server`
+OLDPORT=`grep "http.port" $OLDDIR/conf/application.conf | grep -o "[0-9]*"`
+
+if [ $OLDPORT -eq 9000 ]
+then
+PLAYPORT=9100
+else
+PLAYPORT=9000
+fi
+
+
+unzip regal-api/target/universal/regal-api-*zip -d tmp >/dev/null 
 regalServerDir=regal-server.`date  +"%Y%m%d%H%M"`
 mv tmp/regal-api* $regalServerDir
 rm -rf tmp
-sed s/"^http\.port=.*$"/"http\.port=\$PORT"/ regal-server/conf/application.conf > $regalServerDir/conf/application.conf
-echo "New executable available under $regalServerDir. Provide port info at $regalServerDir/conf/application.conf."
+sed -e "s/^http\.port=.*$/http\.port=$PLAYPORT/" regal-server/conf/application.conf > $regalServerDir/conf/application.conf
+rm $ARCHIVE_HOME/regal-server
+ln -s $regalServerDir $ARCHIVE_HOME/regal-server
+
+echo ""
+echo "New executable available under $regalServerDir." 
+echo "Provide port info at $regalServerDir/conf/application.conf." 
+echo "Current port is set to $PLAYPORT"
+echo "Ready to switch from $OLDDIR to $NEWDIR. From old port $OLDPORT to new $PLAYPORT"
+echo ""
+echo "Please update port information in your apache conf to $PLAYPORT!" 
+echo ""
+echo "To perform switch, execute:"  
+echo "sudo service regal-api start"
+echo "./loadCache.sh"
+echo "sudo service apache2 reload"
+echo "kill `cat $OLDDIR/RUNNING_PID`"
+
 cd -
-
-
---
+++ loadCache.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+source variables.conf
+
+INDEXNAME=edoweb
+
+curl -s -XGET localhost:9200/$INDEXNAME/issue,journal,monograph,volume,file/_search -d'{"query":{"match_all":{}},"fields":["/@id"],"size":"50000"}'|grep -o "edoweb:[^\"]*" >$ARCHIVE_HOME/logs/pids.txt
+
+
+curl -s -XGET localhost:9200/$INDEXNAME/journal/_search -d '{"query":{"match_all":{}},"fields":["/@id"],"size":"50000"}'|grep -o "edoweb:[^\"]*" >$ARCHIVE_HOME/logs/journalPids.txt
+
+
+echo "Try to load resources to cache:"
+cat $ARCHIVE_HOME/logs/journalPids.txt
+
+cat $ARCHIVE_HOME/logs/journalPids.txt | parallel curl -s -uedoweb-admin:$PASSWORD -XGET http://localhost:$PLAYPORT/resource/{}/all -H"accept: application/json"  >$ARCHIVE_HOME/logs/initCache-`date +"%Y%m%d"`.log 2>&1
+
