3a4fceef016c6ee4ec2cc25cbbd4262d02fcbb6e
Jan on Test <schnasse@gmx.de>
2015-09-25 15:18:16 +0200

    minor corrections

--- updateAll.sh
+++ updateAll.sh
@@ -3,7 +3,7 @@
 source variables.conf
 
 PLAYPORT=9000
-INDEXNAME=frl
+INDEXNAME=edoweb
 
 curl -s -XGET localhost:9200/$INDEXNAME/part,issue,journal,monograph,volume,file/_search -d'{"query":{"match_all":{}},"fields":["/@id"],"size":"50000"}'|egrep -o "$INDEXNAME:[^\"]*" >$ARCHIVE_HOME/logs/pids.txt
 
@@ -16,7 +16,7 @@ cat $ARCHIVE_HOME/logs/journalPids.txt
 
 cat $ARCHIVE_HOME/logs/journalPids.txt | parallel curl -s -uedoweb-admin:$PASSWORD -XGET http://localhost:$PLAYPORT/resource/{}/all -H"accept: application/json"  >$ARCHIVE_HOME/logs/initCache-`date +"%Y%m%d"`.log 2>&1
 
-num=`cat $ARCHIVE_HOME/logs/pids.txt|wc-l`
+num=`cat $ARCHIVE_HOME/logs/pids.txt|wc -l`
 
 echo "Update $num resources. Details under $ARCHIVE_HOME/logs/pids.txt." 
 
