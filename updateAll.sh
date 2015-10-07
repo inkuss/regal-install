e4497569976bfc12cb72a7caa961ec74446cb2b3
Jan Schnasse <jan@schnasse.eu>
2015-10-07 13:54:10 +0200

    loadCache, lobidify, enrich, index

--- updateAll.sh
+++ updateAll.sh
@@ -7,17 +7,13 @@ INDEXNAME=edoweb
 
 curl -s -XGET localhost:9200/$INDEXNAME/part,issue,journal,monograph,volume,file/_search -d'{"query":{"match_all":{}},"fields":["/@id"],"size":"50000"}'|egrep -o "$INDEXNAME:[^\"]*" >$ARCHIVE_HOME/logs/pids.txt
 
-
 curl -s -XGET localhost:9200/$INDEXNAME/journal/_search -d '{"query":{"match_all":{}},"fields":["/@id"],"size":"50000"}'|grep -o "$INDEXNAME:[^\"]*" >$ARCHIVE_HOME/logs/journalPids.txt
 
-
-echo "Try to load resources to cache:"
-cat $ARCHIVE_HOME/logs/journalPids.txt
-
+num=`cat $ARCHIVE_HOME/logs/journalPids.txt|wc -l`
+echo "Try to load $num journal resources to cache:"
 cat $ARCHIVE_HOME/logs/journalPids.txt | parallel curl -s -uedoweb-admin:$PASSWORD -XGET http://localhost:$PLAYPORT/resource/{}/all -H"accept: application/json"  >$ARCHIVE_HOME/logs/initCache-`date +"%Y%m%d"`.log 2>&1
 
 num=`cat $ARCHIVE_HOME/logs/pids.txt|wc -l`
-
 echo "Update $num resources. Details under $ARCHIVE_HOME/logs/pids.txt." 
 
 echo "lobidify"
